23da8b3c2a581ac366cabeee09c0d50e982359b1
zimbara <67017679+zimbara14@users.noreply.github.com>
2021-11-23 00:43:41 +0300

    Updated JS file for all the pages.

--- JS/active.js
+++ JS/active.js
@@ -2,12 +2,10 @@ window.addEventListener("DOMContentLoaded", function(event) {
     var links = document.getElementsByClassName("header__menu-link");
     var url = document.location.pathname.split("/");
     var curr = url[url.length - 1];
-    var firstPage = links.item(0).href.split("/")
-    var secondPage = links.item(5).href.split("/")
-    if(firstPage[firstPage.length - 1] === curr) {
-        links.item(0).classList.add('header__menu-item_active')
-    }
-    if(secondPage[secondPage.length - 1] === curr) {
-        links.item(5).classList.add('header__menu-item_active')
+    for(let i = 0; i < links.length; i++){
+        let temp = links.item(i).href.split("/")
+        if(curr == temp[temp.length - 1]) {
+            links.item(i).classList.add('header__menu-item_active')
+        }
     }
 });
