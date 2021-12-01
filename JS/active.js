e4e664fa56947c785d2924f4ee4a29a3cea7097e
zimbara <67017679+zimbara14@users.noreply.github.com>
2021-12-01 15:06:38 +0300

    Fixed script for active menu.

--- JS/active.js
+++ JS/active.js
@@ -1,11 +1,8 @@
 window.addEventListener("DOMContentLoaded", function(event) {
-    var links = document.getElementsByClassName("header__menu-link");
-    var url = document.location.pathname.split("/");
-    var curr = url[url.length - 1];
-    for(let i = 0; i < links.length; i++){
-        let temp = links.item(i).href.split("/")
-        if(curr == temp[temp.length - 1]) {
-            links.item(i).classList.add('header__menu-item_active')
+    let link = document.getElementsByClassName("header__menu-link");
+    for(let i = 0; i < link.length; i++) {
+        if(link.item(i).href === document.location.href) {
+            link.item(i).classList.add('header__menu-item_active')
         }
     }
 });
