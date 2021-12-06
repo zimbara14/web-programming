04c6d7a180689636606cd1779749cf7a95049cf5
zimbara <67017679+zimbara14@users.noreply.github.com>
2021-12-07 00:35:24 +0300

    Fixed script of mockdata page.

--- JS/mock.js
+++ JS/mock.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.random-user__button');
 const template = document.querySelector('#template-row');
-const table = document.querySelector('.random-user__table');
+const table = document.querySelector('.table');
 const loader = document.querySelector('.random-user__loader');
 const error = document.querySelector('.random-user__error');
 
@@ -15,16 +15,6 @@ function rndGenerator(seed) {
 }
 
 function getData() {
-
-    function refresh() {
-        table.style.display = 'none';
-        table.textContent = '';
-        button.style.display = 'none';
-        error.style.display = 'none';
-        loader.style.display = 'inherit';
-    }
-
-    refresh();
     return Promise.all([
         fetch(rndGenerator(1)),
         fetch(rndGenerator(2)),
@@ -42,35 +32,39 @@ function getData() {
         }));
     }).then(function (data) {
         return data;
-    }).then(displayElements);
-    //.catch(displayError)
+    }).then(displayElements).catch(displayError);
 }
 
 function displayElements(data) {
 
+    function refresh() {
+        console.log("we good");
+        table.style.display = 'none';
+        button.style.display = 'none';
+        error.style.display = 'none';
+        loader.style.display = 'inherit';
+        document.querySelectorAll(".random-user__data.table__cell.userId").forEach(el => el.remove());
+        document.querySelectorAll(".random-user__data.table__cell.id").forEach(el => el.remove());
+        document.querySelectorAll(".random-user__data.table__cell.title").forEach(el => el.remove());
+        document.querySelectorAll(".random-user__data.table__cell.body").forEach(el => el.remove());
+    }
+
     function makeHtmlElems() {
-        // manipulate data
-        /*
-        for (let i in data) {
-            let arr = i.value;
-            //console.log(arr);
-            for(let a in arr) {
-                //console.log("are we here? " + a.valueOf());
-            }
+        for (let i = 0; i < 10; i++) {
             const clone = template.content.cloneNode(true);
             const user = clone.querySelector('.userId');
             const id = clone.querySelector('.id');
             const title = clone.querySelector('.title');
             const body = clone.querySelector('.body');
-            user.textContent = data[i]['userId'];
-            id.textContent = data[i]['id'];
-            title.textContent = data[i]['title'];
-            body.textContent = data[i]['body'];
+            user.textContent = data[i][0]['userId'];
+            id.textContent = data[i][0]['id'];
+            title.textContent = data[i][0]['title'];
+            body.textContent = data[i][0]['body'];
             table.appendChild(clone);
         }
-        */
     }
 
+    refresh();
     makeHtmlElems();
     loader.style.display = 'none';
     table.style.display = 'grid';
