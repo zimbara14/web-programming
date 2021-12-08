b4a753b30fe4d30b0e353951f70fa7aac828987a
zimbara <67017679+zimbara14@users.noreply.github.com>
2021-12-08 16:02:48 +0300

    Customized popup window for delete button.

--- JS/lib.js
+++ JS/lib.js
@@ -26,13 +26,17 @@ function getPicture() {
                         .then((value) => {
                             switch (value) {
                                 case "white":
-                                    swal("Ура!", "Ты выбрал белую собаку!",  "success");
+                                    swal("Ура!", "Ты выбрал белую собаку!",  "success", {
+                                        button: "Круто!",
+                                    });
                                     document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
                                     document.querySelector('.img-dog-w').style.display = 'block';
                                     break;
 
                                 case "black":
-                                    swal("Ура!", "Ты выбрал черную собаку!", "success");
+                                    swal("Ура!", "Ты выбрал черную собаку!", "success", {
+                                        button: "Прикольно!",
+                                    });
                                     document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
                                     document.querySelector('.img-dog-b').style.display = 'block';
                                     break;
@@ -50,13 +54,17 @@ function getPicture() {
                         .then((value) => {
                             switch (value) {
                                 case "white":
-                                    swal("Ура!", "Ты выбрал белую кошку!", "success");
+                                    swal("Ура!", "Ты выбрал белую кошку!", "success", {
+                                        button: "Прекрасно!",
+                                    });
                                     document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
                                     document.querySelector('.img-cat-w').style.display = 'block';
                                     break;
 
                                 case "black":
-                                    swal("Ура!", "Ты выбрал черную кошку!", "success");
+                                    swal("Ура!", "Ты выбрал черную кошку!", "success", {
+                                        button: "Удивительно!",
+                                    });
                                     document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
                                     document.querySelector('.img-cat-b').style.display = 'block';
                                     break;
@@ -72,9 +80,27 @@ function getPicture() {
 }
 
 function deleteAll() {
-    document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
-    document.querySelector('.img-del').style.display = 'block';
-    document.querySelector('#butt-del').style.display = 'none';
+    swal({
+        title: "Ты уверен?",
+        text: "Фотка исчезнет!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Пуф!", "Фотки животного больше нет.", {
+                    icon: "success",
+                });
+                document.querySelectorAll(".img").forEach(el => el.style.display = 'none');
+                document.querySelector('.img-del').style.display = 'block';
+                document.querySelector('#butt-del').style.display = 'none';
+            } else {
+                swal("Ура!", "Фотка остается! :)", {
+                    icon: "info",
+                });
+            }
+        });
 }
 
 function onStart() {
